6d5b98d4551849ec6972d3002d1dac877790ced2
RTSaitama <rtsaitama@outlook.com>
2025-04-06 16:20:46 +0300

    discount separated value added

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,16 +7,17 @@
 function calculateRentalCost(days) {
   const rentPerDay = 40;
   const rentPeriod = days;
+  let discount = 50;
 
-  if (rentPeriod >= 7) {
-    return rentPerDay * rentPeriod - 50;
+  if (rentPeriod < 7) {
+    discount = 20;
   }
 
-  if (rentPeriod >= 3) {
-    return rentPerDay * rentPeriod - 20;
+  if (rentPeriod < 3) {
+    discount = 0;
   }
 
-  return rentPerDay * rentPeriod;
+  return rentPerDay * rentPeriod - discount;
 }
 
 module.exports = calculateRentalCost;
