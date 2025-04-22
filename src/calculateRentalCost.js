4a509586ad07844ee8ee16400d216c2f9602aced
RTSaitama <rtsaitama@outlook.com>
2025-04-22 15:01:49 +0300

    naming for discounts

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,18 +1,18 @@
 const SHORT_TERM = 3;
 const LONG_TERM = 7;
 const RENT_PER_DAY = 40;
-const BIG_DISCOUNT = 50;
-const MID_DISCOUNT = 20;
+const LONG_TERM_DISCOUNT = 50;
+const SHORT_TERM_DISCOUNT = 20;
 
 function calculateRentalCost(days) {
   const rentPeriod = days;
 
   if (rentPeriod >= LONG_TERM) {
-    return RENT_PER_DAY * rentPeriod - BIG_DISCOUNT;
+    return RENT_PER_DAY * rentPeriod - LONG_TERM_DISCOUNT;
   }
 
   if (SHORT_TERM <= rentPeriod && rentPeriod < LONG_TERM) {
-    return RENT_PER_DAY * rentPeriod - MID_DISCOUNT;
+    return RENT_PER_DAY * rentPeriod - SHORT_TERM_DISCOUNT;
   }
 
   if (rentPeriod < SHORT_TERM) {
